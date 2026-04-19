1e96bed6179ecea1d56c932c8b1e9ba8d7cfdcf9
Fizza-prog <fizzahassaan84@gmail.com>
2026-04-19 05:22:20 -0700

    practice variables and string concatenation

---
+++ triviaBot.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "CodeBot";
+let botLocation = "Internet";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " is used to build interactive websites.";
+console.log(codingFact);
+
+codingFact = "Many developers love " + favoriteLanguage + " because it's flexible.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " can run in both browser and server environments.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
