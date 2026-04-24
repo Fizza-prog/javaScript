3f1b522c36b4440472a232298ffe34afd5cc9501
Fizza-prog <fizzahassaan84@gmail.com>
2026-04-24 05:20:17 -0700

    add a matching object filter code

---
+++ matchingObjectFilter.js
@@ -0,0 +1,8 @@
+function whatIsInAName(collection, source) {
+  const sourceKeys = Object.keys(source);
+  return collection.filter(obj => {
+    return sourceKeys.every(key => {
+      return obj.hasOwnProperty(key) && obj[key] === source[key];
+    });
+  });
+}
